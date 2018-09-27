81f246b3d4c6c985eaabe0d1a676ad2fbe92a749
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2018-09-27 08:26:27 +0530

    Added handwritten digit classifier using Tensorflow

--- Day-9/playground.py
+++ Day-9/playground.py
@@ -13,3 +13,16 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('mlbootcamp5_train.csv', sep=';')
 
 x= df.iloc[: , :].values
+
+
+my_matrix = np.array([
+    [1,2,3],
+    [4,5,6]
+])
+
+
+my_transpose_matrix = zip(*my_matrix)
+
+
+for row in my_transpose_matrix:
+    print(row)
